3938f6b3cb93e7c9d15c5dd6738966ba819986b0
edon lopez <edson_lopez@hotmail.com.ar>
2017-10-15 10:37:11 -0400

    first commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MATERIAL_COMPATIBILITY_MODE,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatToolbarModule,
+  MatCheckboxModule,
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatListModule
+  ],
+  providers: [
+    { provide: MATERIAL_COMPATIBILITY_MODE, useValue: true }
+  ],
+  declarations: [
+    
+  ]
+})
+export class MaterialModule { }
